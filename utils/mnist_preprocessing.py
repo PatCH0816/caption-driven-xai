97e4c42d0a731f67e97d7f7c2dd3105e482d291b
Patrick Koller <33456180+PatCH0816@users.noreply.github.com>
2023-01-15 17:48:29 -0600

    Balance datasets correctly

--- utils/mnist_preprocessing.py
+++ utils/mnist_preprocessing.py
@@ -3,6 +3,7 @@ from PIL import Image
 import numpy as np
 import torch
 from torchvision import datasets, transforms
+from sklearn.model_selection import train_test_split
 
 
 class align_emnist_like_mnist:
@@ -57,13 +58,10 @@ def grayscale_3d_arr(arr):
 
 class DatasetMNIST(datasets.VisionDataset):
   """
-  Downloads the grayscale MNIST dataset and transforms it into a colored MNIST dataset.
-  Digits smaller than 5 are colored red for the train and validation set. Numbers larger
-  than 5 are colored green for the train and validation set. The colors of the digits have
-  a 50% probability to be flipped.
+  Downloads the grayscale MNIST dataset and transforms it into a colored MNIST dataset with dynamic configurations.
   """
   def __init__(self, root='./data', env='train', opt_postfix="", transform=None, target_transform=None, color=True, filter=range(10),
-               first_color_max_nr=5, preprocess=None):
+               first_color_max_nr=5, preprocess=None, test_fool_random=False):
     super(DatasetMNIST, self).__init__(root, transform=transform,
                                 target_transform=target_transform)
     self.color = color
@@ -72,14 +70,15 @@ class DatasetMNIST(datasets.VisionDataset):
     self._first_color_max_nr = first_color_max_nr
     self._opt_postfix = opt_postfix
     self._preprocess = preprocess
+    self._test_fool_random = test_fool_random
     
     self.prefix = 'color_' if self.color else 'grey_'
 
     self.prepare_colored_mnist()
-    if env in ['train', 'val', 'test', 'test_fool']:
+    if env in ['train', 'test', 'test_fool']:
       self.data_label_tuples = torch.load(os.path.join(self.root, 'mnist', f"{env}_{self.prefix}{self._opt_postfix}.pt"))
     else:
-      raise RuntimeError(f'{env} env unknown. Valid envs are train, val and test')
+      raise RuntimeError(f'{env} env unknown. Valid envs are train, test and test_fool.')
 
   def __getitem__(self, index):
     """
@@ -102,51 +101,59 @@ class DatasetMNIST(datasets.VisionDataset):
 
   def prepare_colored_mnist(self):
     """
-    Download 60'000 grayscale MNIST images, split them into a train, validation and a test
-    dataset and transform them to a colored MNIST dataset.
+    Download 60'000 grayscale MNIST images, split them into a train, test and test_fool
+    datasets and transform them to a colored MNIST dataset.
     """
     
     def mnist_grayscale_to_color():
       # http://yann.lecun.com/exdb/mnist/
       # try balanced emnist: https://www.nist.gov/itl/products-and-services/emnist-dataset
       print('Preparing Colored MNIST')
-      train_mnist = datasets.mnist.EMNIST(self.root, train=True, download=True, split="mnist", transform = transforms.Compose([transforms.ToTensor(),
-                                                                                                                               align_emnist_like_mnist()])) # 60'000 samples for training and validation
-      test_mnist = datasets.mnist.EMNIST(self.root, train=False, download=True, split="mnist", transform = transforms.Compose([transforms.ToTensor(),
-                                                                                                                               align_emnist_like_mnist()])) # 10'000 samples for test
-      # train_mnist = datasets.mnist.MNIST(self.root, train=True, download=True, transform = transforms.Compose([transforms.ToTensor()])) # 60'000 samples for training and validation
+      train_mnist = datasets.mnist.EMNIST(self.root,
+                                          train=True,
+                                          download=True,
+                                          split="mnist",
+                                          transform = transforms.Compose([transforms.ToTensor(),
+                                                                          align_emnist_like_mnist()])) # 60'000 samples for training
+      
+      test_mnist = datasets.mnist.EMNIST(self.root,
+                                         train=False,
+                                         download=True,
+                                         split="mnist",
+                                         transform = transforms.Compose([transforms.ToTensor(),
+                                                                         align_emnist_like_mnist()])) # 10'000 samples for test
+      
+      # train_mnist = datasets.mnist.MNIST(self.root, train=True, download=True, transform = transforms.Compose([transforms.ToTensor()])) # 60'000 samples for training
       # test_mnist = datasets.mnist.MNIST(self.root, train=False, download=True, transform = transforms.Compose([transforms.ToTensor()])) # 10'000 samples for test
       
+      # split training dataset in smaller training dataset and test-fool/real-world dataset
+      train_test_fool = train_test_split(train_mnist, train_size=50000, stratify=train_mnist.train_labels)
+      
       # shuffle datasets
-      train_loader = torch.utils.data.DataLoader(train_mnist,
-                                                batch_size=1,
-                                                shuffle=True)
+      train_loader = torch.utils.data.DataLoader(train_test_fool[0],
+                                                  batch_size=1,
+                                                  shuffle=True)
+      
+      test_fool_loader = torch.utils.data.DataLoader(train_test_fool[1],
+                                                      batch_size=1,
+                                                      shuffle=True)
+      
       test_loader = torch.utils.data.DataLoader(test_mnist,
                                                 batch_size=1,
                                                 shuffle=True)
       
       # setup
-      environment = {"train":      {"dataset":train_loader},
-                      "validation": {"dataset":train_loader},
-                      "test":       {"dataset":test_loader},
-                      "test_fool":  {"dataset":test_loader}}
+      environment = {"train":       {"dataset":train_loader},
+                      "test_fool":  {"dataset":test_fool_loader},
+                      "test":       {"dataset":test_loader}}
       
-      # split train/validation
-      train_size = 50000
-
       train_set = []
-      validation_set = []
       test_set = []
       test_fool_set = []
             
       for phase in environment.keys():        
         for idx, (im, ground_truth) in enumerate(environment[phase]["dataset"]):   
-          # turn training data into train/validation datasets
-          if (phase == "train" and idx >= train_size):
-            continue
-          if (phase == "validation" and idx < train_size):
-            continue
-          
+
           # progress bar
           if ((idx % 5000) == 0):
               print(f'Scanning {phase} image {idx}/{len(environment[phase]["dataset"])}')
@@ -162,12 +169,13 @@ class DatasetMNIST(datasets.VisionDataset):
             # Assign random color labels to test set
             if phase == 'train':
               color_label = low_high_label
-            elif phase == 'validation':
-              color_label = low_high_label
             elif phase == 'test':
               color_label = low_high_label
             elif phase == 'test_fool':
-              color_label = low_high_label^1 #np.random.choice([0,1])
+              if self._test_fool_random:
+                color_label = np.random.choice([0,1])
+              else:
+                color_label = low_high_label^1
             else:
               raise Exception("Oops.. Unkown phase!")
             
@@ -185,28 +193,24 @@ class DatasetMNIST(datasets.VisionDataset):
           # color_label (potentially mixed up label for test dataset)
           if phase == 'train':
             train_set.append((Image.fromarray((new_image * 255).astype(np.uint8)), ground_truth, low_high_label, color_label))
-          elif phase == 'validation':
-            validation_set.append((Image.fromarray((new_image * 255).astype(np.uint8)), ground_truth, low_high_label, color_label))
           elif phase == 'test':
             test_set.append((Image.fromarray((new_image * 255).astype(np.uint8)), ground_truth, low_high_label, color_label))
           elif phase == 'test_fool':
             test_fool_set.append((Image.fromarray((new_image * 255).astype(np.uint8)), ground_truth, low_high_label, color_label))
                
-      return train_set, validation_set, test_set, test_fool_set
+      return train_set, test_set, test_fool_set
       
     # do files already exist?
     colored_mnist_dir = os.path.join(self.root, 'mnist')
     if os.path.exists(os.path.join(colored_mnist_dir, f"train_{self.prefix}{self._opt_postfix}.pt")) \
-      and os.path.exists(os.path.join(colored_mnist_dir, f"val_{self.prefix}{self._opt_postfix}.pt")) \
       and os.path.exists(os.path.join(colored_mnist_dir, f"test_{self.prefix}{self._opt_postfix}.pt")) \
       and os.path.exists(os.path.join(colored_mnist_dir, f"test_fool_{self.prefix}{self._opt_postfix}.pt")):
       print('MNIST dataset already exists')
       return
     
-    train_set, val_set, test_set, test_fool_set = mnist_grayscale_to_color()
+    train_set, test_set, test_fool_set = mnist_grayscale_to_color()
 
     os.makedirs(colored_mnist_dir, exist_ok=True)
     torch.save(train_set, os.path.join(colored_mnist_dir, f"train_{self.prefix}{self._opt_postfix}.pt"))
-    torch.save(val_set, os.path.join(colored_mnist_dir, f"val_{self.prefix}{self._opt_postfix}.pt"))
     torch.save(test_set, os.path.join(colored_mnist_dir, f"test_{self.prefix}{self._opt_postfix}.pt"))
     torch.save(test_fool_set, os.path.join(colored_mnist_dir, f"test_fool_{self.prefix}{self._opt_postfix}.pt"))
